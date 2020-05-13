23bb8f4f8f5332788d18fb7077c40dc27c351730
Angela Venger <venger@Angelas-MacBook-Pro.local>
2020-05-13 11:53:46 -0700

    refactoring with react hooks

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,11 +1,11 @@
-import React from "react"
-import UserItem from "./UserItem"
-import Spinner from "../layout/Spinner"
-import PropTypes from "prop-types"
+import React from 'react';
+import UserItem from './UserItem';
+import Spinner from '../layout/Spinner';
+import PropTypes from 'prop-types';
 
 const Users = ({ users, loading }) => {
   if (loading) {
-    return <Spinner />
+    return <Spinner />;
   } else {
     return (
       <div style={userStyle}>
@@ -13,18 +13,18 @@ const Users = ({ users, loading }) => {
           <UserItem key={user.id} user={user} />
         ))}
       </div>
-    )
+    );
   }
-}
+};
 
 Users.propTypes = {
   users: PropTypes.array.isRequired,
   loading: PropTypes.bool.isRequired,
-}
+};
 
 const userStyle = {
-  display: "grid",
-  gridTemplateColumns: "repeat(3,1fr)",
-  gridGap: "1rem",
-}
-export default Users
+  display: 'grid',
+  gridTemplateColumns: 'repeat(3,1fr)',
+  gridGap: '1rem',
+};
+export default Users;
