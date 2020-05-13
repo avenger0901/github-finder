8e321d041bf90a2c4b92c9c381541dd08a571986
Angela Venger <venger@Angelas-MacBook-Pro.local>
2020-05-13 10:21:59 -0700

    add repo details

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,14 +1,18 @@
 import React, { Fragment, Component } from 'react';
 import PropTypes from 'prop-types';
+import Repos from '../repos/Repos';
 import { Link } from 'react-router-dom';
 export class User extends Component {
   componentDidMount() {
     this.props.getUser(this.props.match.params.login);
+    this.props.getUserRepos(this.props.match.params.login);
   }
 
   static propTypes = {
     user: PropTypes.object.isRequired,
+    repos: PropTypes.array.isRequired,
     getUser: PropTypes.func.isRequired,
+    getUserRepos: PropTypes.func.isRequired,
   };
   render() {
     const {
@@ -27,7 +31,7 @@ export class User extends Component {
       hireable,
     } = this.props.user;
 
-    const { loading } = this.props;
+    const { loading, repos } = this.props;
 
     return (
       <Fragment>
@@ -92,6 +96,7 @@ export class User extends Component {
           <div className="badge badge-light">Public Repos: {public_repos}</div>
           <div className="badge badge-dark">Public Gist : {public_gists}</div>
         </div>
+        <Repos repos={repos} />
       </Fragment>
     );
   }
